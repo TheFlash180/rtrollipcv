7ad4f3125dbdf34e528b54c0fd22b35f9c6b1636
TheFlash180 <rickust18@gmail.com>
2020-09-17 14:09:06 +0200

    Testing buttons, text and styles

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Welcome to my online CV</h1>`,
-  styles: [`h1 { font-family: Lato; text-align: center;}`]
+  template: `<h1> Welcome to my online CV</h1>`,
+  styles: [`h1 { font-family: Lato; text-align: center; font-size: 300%;}`]
 })
 export class HelloComponent  {
   @Input() name: string;
