00fa598ac5ff07a27b0c020e09a5bc1625d96c3d
TheFlash180 <rickust18@gmail.com>
2020-09-17 13:27:50 +0200

    Initial commit

---
+++ src/app/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Welcome to my online CV</h1>`,
+  styles: [`h1 { font-family: Lato; text-align: center;}`]
+})
+export class HelloComponent  {
+  @Input() name: string;
+}
