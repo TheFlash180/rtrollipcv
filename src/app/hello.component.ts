9b09896a915d9c914085311853ede9668b80fd7b
TheFlash180 <rickust18@gmail.com>
2020-09-28 17:21:07 +0200

    Added Navigation bar with logo, links and button, also added new background as well as some animation.

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1> Welcome to my online CV</h1>`,
+  template: ``,
   styles: [`h1 { font-family: Lato; text-align: center; font-size: 300%;}`]
 })
 export class HelloComponent  {
